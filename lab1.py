61c21aa89f6528eab21efd322b38986a362427e9
dafnaer23-meet <109908294+dafnaer23-meet@users.noreply.github.com>
2022-07-25 18:17:36 +0300

    hhhh

--- lab1.py
+++ lab1.py
@@ -1,5 +1,24 @@
 def create_youtube_video(title, discription,lik):
 	dictvideo={"title":title,"discription":discription,"likes":0,"dislikes":0,"comments":0}
-new_video=create_youtube_video("me", "the story of my life", dictvideo("likes")=100, dictvideo("dislikes")=1, dictvideo("comments")=90)
+    return dictvideo
 
-	
+
+def like(dictvideo):
+	if "likes" in dictvideo:
+		dictvideo["likes"] = dictvideo["likes"]+1
+	return dictvideo
+
+def dislike(dictvideo):
+	if "dislikes" in dictvideo:
+		dictvideo["dislikes"] = dictvideo["dislikes"]+1
+	return dislike
+
+def add comments(youtubevideo, username, comment_text):
+	youtubevideo["comments"][username] = comment_text
+	return youtubevideo
+
+new video =create_youtube_video("paint", "more paints")
+for i in range(495):
+	new_video = like(new_video)
+new_video = dislike(new_video, "lia", "love it")
+print(new_video)
---
+++ pythonReview.py
@@ -0,0 +1,5 @@
+def create_youtube_video(title, discription,lik):
+	dictvideo={"title":title,"discription":discription,"likes":0,"dislikes":0,"comments":0}
+new_video=create_youtube_video("me", "the story of my life", dictvideo("likes")=100, dictvideo("dislikes")=1, dictvideo("comments")=90)
+
+	
