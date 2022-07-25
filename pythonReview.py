97addee40eaa772865e49696c19683e9aacb2b87
dafnaer23-meet <109908294+dafnaer23-meet@users.noreply.github.com>
2022-07-25 18:19:22 +0300

    updated version

--- pythonReview.py
+++ pythonReview.py
@@ -1,5 +1,28 @@
-def create_youtube_video(title, discription,lik):
-	dictvideo={"title":title,"discription":discription,"likes":0,"dislikes":0,"comments":0}
-new_video=create_youtube_video("me", "the story of my life", dictvideo("likes")=100, dictvideo("dislikes")=1, dictvideo("comments")=90)
+
+def create_youtube_video(title, discription):
+	dictvideo={"title":title,"discription":discription,"likes":0,"dislikes":0,"comments":{}}
+	return dictvideo
+
+
+def like(dictvideo):
+	if "likes" in dictvideo:
+		dictvideo["likes"] = dictvideo["likes"]+1
+	return dictvideo
+
+def dislike(dictvideo):
+	if "dislikes" in dictvideo:
+		dictvideo["dislikes"] = dictvideo["dislikes"]+1
+	return dislike
+def add_comment(dictvideo, username, comment_text):
+	dictvideo["comments"][username] = comment_text
+	return dictvideo
+
+myvideo = create_youtube_video("me", "who i am")
+print(myvideo)
+like(myvideo)
+dislike(myvideo)
+print(myvideo)
+add_comment(myvideo,"roni", "nice video!!xoxoxoxo")
+print(myvideo)
 
 	
